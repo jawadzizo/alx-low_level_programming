cd488b8b1e2152fcc7cbbe3850121874ed09a5a1
jawadzizo <jadosss99@gmail.com>
2023-07-15 04:27:41 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - a function to print the phrase bellow without using printf
+ *
+ * Return: returns 1
+ */
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	long unsigned i = 0;
+	unsigned long i = 0;
 
 	while (i <= sizeof(quote))
 	{
