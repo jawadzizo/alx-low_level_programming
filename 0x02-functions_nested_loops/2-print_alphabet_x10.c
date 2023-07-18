4480c70cabf8cd7ad8a1480658290a4f7b7cefcb
jawadzizo <jadosss99@gmail.com>
2023-07-18 11:22:45 -0700

    added 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,12 @@
 
 void print_alphabet_x10(void)
 {
-	
-	for (int c = 0; c <= 9; c++)
+	int i;
+	int c;
+
+	for (c = 0; c <= 9; c++)
 	{
-		for (int i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 
 		if (c != 9)	
