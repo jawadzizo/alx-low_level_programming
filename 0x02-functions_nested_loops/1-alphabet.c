8fd8785571c56c392150be74367062148eab45f4
jawadzizo <jadosss99@gmail.com>
2023-07-19 09:01:36 -0700

    added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - a function to print lowercase characters
- *
- * Return: return 0
+ * print_alphabet - a function to print alphabets
  */
-/** print_alphabet - a function to print alphabets*/
 void print_alphabet(void)
 {
 	char i;
