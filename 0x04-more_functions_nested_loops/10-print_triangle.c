77b0918a5cedd407effeb116e1bd504ef8528637
jawadzizo <jadosss99@gmail.com>
2023-07-21 04:08:05 -0700

    added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - a function to print a triangle
+ *
+ * @n: how many lines in this triangle
+ */
+
+void print_diagonal(int n)
+{
+	int i;       /* space */
+	int j;       /* # */
+	int k = n + 1;
+
+	for (; n > 0; n--)
+	{
+		for (i = n - 1; i > 0; i--)
+			_putchar(' ');
+
+		for (j = 0; j < k - n; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
