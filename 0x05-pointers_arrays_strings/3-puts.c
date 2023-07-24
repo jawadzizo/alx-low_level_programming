6c99cf8568a107a81cffd3b471f686fa2172711c
jawadzizo <jadosss99@gmail.com>
2023-07-24 12:30:03 -0700

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - a function to print a string
+ *
+ * @str: the string to be printed
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
+}
