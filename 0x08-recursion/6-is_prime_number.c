7bc76de90861dd201e296cd14d6b0e178e965fc5
jawadzizo <jadosss99@gmail.com>
2023-08-02 14:34:44 -0700

    added 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_number(int n)
 	if (n == 1 || n == -1)
 	return (0);
 
-    else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
 	return (0);
 
 	else
