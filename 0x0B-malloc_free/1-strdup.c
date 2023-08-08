5e06e8856b21619b613237f1b243302417986ff0
jawadzizo <jadosss99@gmail.com>
2023-08-08 11:30:22 -0700

    added 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,22 @@
 
 char *_strdup(char *str)
 {
-	char *string = (char *)malloc(sizeof(str));
-	unsigned int i;
+	char *string;
+	int i;
+	int len;
+	int j = 0;
 
 	if (str == NULL)
 	return (NULL);
 
-	for (i = 0; i < sizeof(str); i++)
+	for (len = 0; *(str + j) != '\0'; len++)
+	{
+		j++;
+	}
+
+	string = (char *)malloc(len);
+
+	for (i = 0; i < len; i++)
 	string[i] = str[i];
 
 	return (string);
