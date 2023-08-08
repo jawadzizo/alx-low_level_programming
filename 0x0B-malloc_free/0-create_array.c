dbc1e85e1bd44635d16b883859dc9afcf76ea24b
jawadzizo <jadosss99@gmail.com>
2023-08-08 09:51:54 -0700

    added 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str = (char *)malloc(size);
+	int i;
 
 	if (size == 0)
 	return (NULL);
 
-	str[0] = c;
+	for (i = 0; i < size; i++)
+	str[i] = c;
 
 	return (str);
 }
