3dd2dd0ef050d278238acb320b6ed9429f43dcfe
jawadzizo <jadosss99@gmail.com>
2023-08-10 14:51:54 -0700

    added 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,11 +13,13 @@ unsigned int _strspn(char *s, char *accept)
 	int j;
 	unsigned int len;
 
+	len = 0;
+
 	for (i = 0; accept[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (s[i] == s[j])
+			if (accept[i] == s[j])
 			{
 				len++;
 				break;
