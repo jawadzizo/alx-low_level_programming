331ee26edc1fe768c2b3479f52e79adfc5fd644b
jawadzizo <jadosss99@gmail.com>
2023-08-14 14:39:16 -0700

    added 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,9 +12,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int len = strlen(name) + 1 + sizeof(age) + strlen(owner) + 1;
+	dog_t *hello;
+	int lenname = 0;
+	int lenowner = 0;
+	long int len;
+	int i;
 
-	dog_t *hello = malloc(len);
+	if (name != NULL)
+	{
+		lenname = 1;
+		for (i = 0; name[i] != '\0'; i++)
+		lenname++;
+	}
+
+	if (owner != NULL)
+	{
+		lenowner = 1;
+		for (i = 0; owner[i] != '\0'; i++)
+		lenowner++;
+	}
+
+	len = lenname + sizeof(age) + lenowner;
+
+	hello = malloc(len);
 
 	hello->name = name;
 	hello->age = age;
