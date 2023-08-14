659270cb9b0b44de614b2e2aeb9bd5873e2db39f
jawadzizo <jadosss99@gmail.com>
2023-08-14 12:55:59 -0700

    added dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,8 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * dog - a struct
+ * struct dog - a struct
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -12,3 +15,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+
+
+
+
+
+
+
+
+#endif
