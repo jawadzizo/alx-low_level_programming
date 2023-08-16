ddc1196833c1b4175e2d3c57de8003cab9fdd585
jawadzizo <jadosss99@gmail.com>
2023-08-16 09:36:44 -0700

    added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function to print a name using another function.
+ * @name: the name to print.
+ * @f: the function to use.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
