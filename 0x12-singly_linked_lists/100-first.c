c8f42168ea21d820e50dc206cb10fd7f8b2e2f9a
jawadzizo <jadosss99@gmail.com>
2023-08-25 12:47:37 -0700

    added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * before - a function to print a string before the main function.
+ */
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
