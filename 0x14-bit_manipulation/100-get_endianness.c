2e459ea9f587af0ab7ab3231b12b0b7eec9e4bb5
jawadzizo <jadosss99@gmail.com>
2023-11-23 13:44:50 -0800

    added 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - a function to detect machine's endianness.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	int number = 1;
+	char *ptr = (char*)&number;
+
+	if (*ptr)
+		return (1);
+
+	else
+		return (0);
+}
