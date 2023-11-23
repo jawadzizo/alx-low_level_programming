eff92416f7f61c741f8d5fcb42845101f6183ab9
jawadzizo <jadosss99@gmail.com>
2023-11-23 12:12:50 -0800

    added 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - a function to detect the bit at a given index.
+ * @n: the number to check.
+ * @index: the index of the bit to be checked.
+ * Return: returns the bit value at the index, or -1 if failed.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = (n >> index) & 1;
+
+	if ((n >> index) <= 0)
+		return (-1);
+
+	return (bit);
+}
