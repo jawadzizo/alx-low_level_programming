03c1a7d699b8a1c97441f38fa6f700d5d917a37c
jawadzizo <jadosss99@gmail.com>
2023-11-24 03:34:33 -0800

    added 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,6 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	int bit = (*n >> index) & 1;
 	int value = 1;
 
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+
 	value = value << index;
 
 	if (bit)
