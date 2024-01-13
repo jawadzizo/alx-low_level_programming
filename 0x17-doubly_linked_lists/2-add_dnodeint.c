5402eca6d84e4012a0ee07084211c0cf4ba9ba0f
jawadzizo <jadosss99@gmail.com>
2024-01-13 06:12:03 -0800

    updated 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,6 +9,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 	{
 		printf("error allocating memory for the new node\n");
