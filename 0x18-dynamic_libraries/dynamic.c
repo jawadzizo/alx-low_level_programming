809aa83f9dca4908d38b395cb4ef92332f58576c
jawadzizo <jadosss99@gmail.com>
2024-01-15 10:44:35 -0800

    added libdynamic.so

---
+++ 0x18-dynamic_libraries/dynamic.c
@@ -0,0 +1,21 @@
+int _putchar(char c __attribute__((unused))){}
+int _islower(int c __attribute__((unused))){}
+int _isalpha(int c __attribute__((unused))){}
+int _abs(int n __attribute__((unused))){}
+int _isupper(int c __attribute__((unused))){}
+int _isdigit(int c __attribute__((unused))){}
+int _strlen(char *s __attribute__((unused))){}
+void _puts(char *s __attribute__((unused))){}
+char *_strcpy(char *dest __attribute__((unused)), char *src __attribute__((unused))){}
+int _atoi(char *s __attribute__((unused))){}
+char *_strcat(char *dest __attribute__((unused)), char *src __attribute__((unused))){}
+char *_strncat(char *dest __attribute__((unused)), char *src __attribute__((unused)), int n __attribute__((unused))){}
+char *_strncpy(char *dest __attribute__((unused)), char *src __attribute__((unused)), int n __attribute__((unused))){}
+int _strcmp(char *s1 __attribute__((unused)), char *s2 __attribute__((unused))){}
+char *_memset(char *s __attribute__((unused)), char b __attribute__((unused)), unsigned int n __attribute__((unused))){}
+char *_memcpy(char *dest __attribute__((unused)), char *src __attribute__((unused)), unsigned int n __attribute__((unused))){}
+char *_strchr(char *s __attribute__((unused)), char c __attribute__((unused))){}
+unsigned int _strspn(char *s __attribute__((unused)), char *accept __attribute__((unused))){}
+char *_strpbrk(char *s __attribute__((unused)), char *accept __attribute__((unused))){}
+char *_strstr(char *haystack __attribute__((unused)), char *needle __attribute__((unused))){}
+
