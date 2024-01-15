c3f41fb3c9ff64917bb443be01dd9db6c16d5481
jawadzizo <jadosss99@gmail.com>
2024-01-15 11:14:25 -0800

    added 100-operations.so

---
+++ 0x18-dynamic_libraries/c_imported.c
@@ -0,0 +1,55 @@
+/**
+ * add - adds 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracts 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the remainder of integer 1 by b
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of the operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
